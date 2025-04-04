4f05b0606f0b4fd5269f48b299b753ec19497055
ETEC Adolpho Berezin <dixxgabriellasam@gmail.com>
2025-04-04 19:59:52 -0300

    Início

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.WriteLine("Escada");
+
+Console.Write("Altura da escada: ");
+double comprimentoEscada = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Angulo em graus: ");
+double anguloEmGraus = Convert.ToDouble(Console.ReadLine());
+
+double anguloEmRadianos = Math.PI * anguloEmGraus / 180;
+double altura = comprimentoEscada * Math.Sin(anguloEmRadianos);
+
+Console.WriteLine($"Uma escada de {comprimentoEscada}m a {anguloEmGraus}° alcança {altura:N}m");
